b27377690e7145bdcd1f9900ae20a2be05d2b69d
Petr Bačovský <petrbacov@gmail.com>
2022-09-30 17:36:00 +0200

    show done

--- js/main.js
+++ js/main.js
@@ -55,3 +55,19 @@ function typing() {
   }
 }
 typing();
+
+const cards = document.querySelectorAll(".contact-box");
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+      if (entry.isIntersecting) observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+cards.forEach((card) => {
+  observer.observe(card);
+});
